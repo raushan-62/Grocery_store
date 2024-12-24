312a4da3df3112ce4b4e1101db95b11a90e4e1c7
Raushan Kumar <23f3004198@ds.study.iitm.ac.in>
2024-12-24 18:31:20 +0530

    Updates of day 2

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    name = 'Raushan'
-    return render_template('index.html',name = name)
+import config
+import routes
+
 
 if __name__ == '__main__':
     app.run(debug = True)
