d9d798d7dcac4e6d0f5962fe12b2195b28022610
Raushan Kumar <23f3004198@ds.study.iitm.ac.in>
2024-12-25 18:32:18 +0530

    Day-3 Updates decorators, authentication and backend

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ import routes
 
 
 if __name__ == '__main__':
-    app.run(debug = True, port = 5500)
+    app.run(debug = True, port = 5000)
--- routes.py
+++ routes.py
@@ -1,10 +1,9 @@
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template, request, redirect, url_for, flash,session
 from app import app
 from models import db, User, Category, Product, Cart, Transaction, Order
 from werkzeug.security import generate_password_hash, check_password_hash
-@app.route('/')
-def index():
-    return render_template('index.html')
+from functools import wraps
+
 
 @app.route('/login')
 def login():
@@ -20,7 +19,8 @@ def login_post():
     if not user or not check_password_hash(user.passhash, password):
         flash("Invalid username or password")
         return redirect(url_for('login'))
-
+    session['user_id'] = user.id
+    flash('You have successfully logged in')
     return redirect(url_for('index'))
 @app.route('/register')
 def register():
@@ -52,5 +52,65 @@ def register_post():
     db.session.commit()
 
     return redirect(url_for('login'))
-    
 
+def auth_required(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        if 'user_id' in session:
+            return func(*args, **kwargs)
+        else:
+            flash('You must be logged in first')
+            return redirect(url_for('login'))
+    return inner
+
+
+@app.route('/profile')
+@auth_required  # decorator for authentication required before accessing this route 
+def profile():
+    if 'user_id' in session:
+        user = User.query.get(session['user_id'])
+        return render_template('profile.html', user = user)
+    else:
+        flash('You must be logged in first')
+        return redirect(url_for('login'))
+
+@app.route('/profile', methods = ['POST'])
+@auth_required
+def profile_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    new_password = request.form.get('new_password')
+    name = request.form.get('name')
+    if not username or not password or not new_password:
+        flash('Please enter all details')
+        return redirect(url_for('profile'))
+    user = User.query.get(session['user_id'])
+    if not check_password_hash(user.passhash, password):
+        flash('Incorrect Password')
+        return redirect(url_for('profile'))
+    if username != user.username:
+        new_username = User.query.filter_by(username = username).first()
+        if new_username:
+            flash('Username already exists')
+            return redirect(url_for('profile'))
+    new_password_hash = generate_password_hash(new_password)
+    user.username = username
+    user.passhash = new_password_hash
+    if name:
+        user.name = name
+    user.name = user.name
+    db.session.commit()
+    flash('Profile updated successfully')
+    return redirect(url_for('profile'))
+@app.route('/')
+@auth_required
+def index():
+
+    return render_template('index.html')
+
+@app.route('/logout')
+@auth_required  # decorator for authentication required before accessing this route 
+def logout():
+    session.pop('user_id', None)
+    flash('You have logged out')
+    return redirect(url_for('login'))
